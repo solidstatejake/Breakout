9d945eadfb1131e9646438e191e54a31dd0466d2
solidstatejake <jake.berg@sunydutchess.edu>
2019-05-21 19:01:45 -0400

    Determine proper screen dimensions to include a border and header.

--- src/Breakout.java
+++ src/Breakout.java
@@ -1,6 +1,5 @@
 
-import acm.graphics.GOval;
-import acm.graphics.GPolygon;
+import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 import java.awt.event.MouseEvent;
@@ -9,56 +8,57 @@ import java.awt.event.MouseMotionListener;
 
 public class Breakout extends GraphicsProgram {
 
-    public void run(){
+    // Constants
+    private final int SCREEN_WIDTH      = 416;
+    private final int SCREEN_HEIGHT     = 716;
+    private final int BALL_RADIUS       = 10;
+    private final int BRICK_WIDTH       = 36;
+    private final int BRICK_HEIGHT      = 8;
+    private final int PADDLE_WIDTH      = 60;
+    private final int PADDLE_HEIGHT     = 10;
+    private final int BALL_SPEED        = 10;
+    private final GPoint canvasCenter = new GPoint(getWidth()/2,
+                                                  getHeight()/2);
+    private final GPoint initialPaddleLocation = new GPoint(100, 200);
+    private final GOval ball = new GOval(BALL_RADIUS, BALL_RADIUS);
+    private final GRect paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
+    private final GRect brick = new GRect(BRICK_WIDTH, BRICK_HEIGHT);
+    private final GRect border = new GRect(400, 600);
 
-
-
-
-        GPolygon a = new GPolygon();
-
-        a.addVertex(-22, 0);
-        a.addVertex(0, 36);
-        a.addVertex(22, 0);
-        a.addVertex(0, -36);
-        a.setLocation(300,400);
-        add(a);
-
-        GPolygon b = new GPolygon();
-        b.addVertex(-22, 0);
-        b.addEdge(22,36);
-        b.addEdge(22,-36);
-        b.addEdge(-22, -36);
-        b.addEdge(-22, 36);
-        b.setLocation(100,100);
-        add(b);
+    private void initializeBorder(){
 
     }
-    /*This method will call:
-     *   makePaddle()
-     *   makeBricks()
-     *   makeBall()          */
+
+    private void initializePaddle(){
+
+    }
+
+    private void intitializeBricks(){
+
+    }
+
+    private void initializeHeader(){
+
+    }
+
+    private void inititalizeBall(){
+
+    }
+
     private void initialize(){
+        // Set screen size.
+        setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 
+        border.setLocation(8, 86);
+        add(border);
 
-    }
-
-
-    private void makePaddle(){
-
-
-    }
-
-
-    private void makeBricks(){
 
 
     }
 
-    private void makeBall() {
-
+    public void run(){
+        initialize();
     }
-
-
 
 
 }
