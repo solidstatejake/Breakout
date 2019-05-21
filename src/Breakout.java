39a771cab9346b2a89c3fc7992c7b454a82527ce
solidstatejake <jake.berg@sunydutchess.edu>
2019-05-21 19:49:17 -0400

    Create intialization method for paddle.

--- src/Breakout.java
+++ src/Breakout.java
@@ -6,26 +6,36 @@ import acm.program.GraphicsProgram;
 public class Breakout extends GraphicsProgram {
 
     // Constants
-    private final int SCREEN_WIDTH      = 416;
-    private final int SCREEN_HEIGHT     = 716;
-    private final int BORDER_WIDTH      = 400;
-    private final int BORDER_HEIGHT     = 600;
-    private final int BALL_RADIUS       = 10;
-    private final int BRICK_WIDTH       = 36;
-    private final int BRICK_HEIGHT      = 8;
-    private final int PADDLE_WIDTH      = 60;
-    private final int PADDLE_HEIGHT     = 10;
-    private final int BALL_SPEED        = 10;
+    private final int SCREEN_WIDTH          = 416;
+    private final int SCREEN_HEIGHT         = 716;
+
+    private final int INNER_BORDER_WIDTH    = 400;
+    private final int INNER_BORDER_HEIGHT   = 600;
+
+    private final int PADDLE_WIDTH          = 60;
+    private final int PADDLE_HEIGHT         = 10;
+    private final double INITIAL_PADDLE_X = (SCREEN_WIDTH - PADDLE_WIDTH) / 2;
+    private final double INITIAL_PADDLE_Y = (SCREEN_HEIGHT - PADDLE_WIDTH)* 0.9;
+    private final GPoint initialPaddleLocation = new GPoint(INITIAL_PADDLE_X,
+                                                            INITIAL_PADDLE_Y);
+    private final GRect paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
+
+
+
+    private final int BALL_RADIUS           = 10;
+
+    private final int BRICK_WIDTH           = 36;
+    private final int BRICK_HEIGHT          = 8;
+
+    private final int BALL_SPEED            = 10;
     private final GPoint canvasCenter = new GPoint(getWidth()/2,
                                                   getHeight()/2);
 
     //Not yet the proper initial paddle location.
-    private final GPoint initialPaddleLocation = new GPoint(100, 200);
     private final GOval ball = new GOval(BALL_RADIUS, BALL_RADIUS);
-    private final GRect paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
     private final GRect brick = new GRect(BRICK_WIDTH, BRICK_HEIGHT);
-    private final GRect innerBorder = new GRect(400, 600);
-    private final GRect outerBorder = new GRect(415, 685);
+    private final GRect innerBorder = new GRect(INNER_BORDER_WIDTH,
+                                                INNER_BORDER_HEIGHT);
 
     private void initializeScreen(){
         setSize(SCREEN_WIDTH,SCREEN_HEIGHT);
@@ -35,12 +45,9 @@ public class Breakout extends GraphicsProgram {
         add(innerBorder);
     }
 
-    private void initializeOuterBorder(){
-
-    }
-
     private void initializePaddle(){
-
+        paddle.setLocation(initialPaddleLocation);
+        add(paddle);
     }
 
     private void intitializeBricks(){
@@ -58,7 +65,7 @@ public class Breakout extends GraphicsProgram {
     private void initialize(){
         initializeScreen();
         initializeInnerBorder();
-
+        initializePaddle();
     }
 
     public void run(){
