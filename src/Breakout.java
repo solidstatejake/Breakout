4115de5bcd4e0c1a66f4427b5b83738c2cf29300
solidstatejake <jake.berg@sunydutchess.edu>
2019-05-21 22:07:00 -0400

    Implement initializeHeader(). Ready to begin implementing gameplay.

--- src/Breakout.java
+++ src/Breakout.java
@@ -16,6 +16,8 @@ public class Breakout extends GraphicsProgram {
     private final int BORDER_HEIGHT = 600;
     private final int BORDER_OFFSET_NORTH = 86; //From screen top to border top.
     private final int BORDER_OFFSET = 8;
+    private final GRect border = new GRect(BORDER_WIDTH, BORDER_HEIGHT);
+
 
     private final int PADDLE_WIDTH          = 60;
     private final int PADDLE_HEIGHT         = 10;
@@ -24,26 +26,25 @@ public class Breakout extends GraphicsProgram {
     private final GRect paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
 
 
+
     private final int BALL_RADIUS           = 10;
     private final int BALL_SPEED            = 10;
+    private final GOval ball = new GOval(BALL_RADIUS, BALL_RADIUS);
+
 
     private final int BRICK_WIDTH           = 36;
     private final int BRICK_HEIGHT          = 15;
     private final int BRICK_SPACING         = 4;
     private final int BRICKS_IN_ROW         = 10;
     private final int BRICKS_IN_COL         = 10;
+    private final int BRICK_OFFSET_X        = 2;
+    private final int BRICK_OFFSET_Y        = 60;
 
 
+    private final double CANVAS_CENTER_X = SCREEN_WIDTH / 2;
+    private final double CANVAS_CENTER_Y = SCREEN_HEIGHT / 2;
 
-    private final GPoint canvasCenter = new GPoint(
-            (SCREEN_WIDTH - BALL_RADIUS)/2,
-            (SCREEN_HEIGHT - BALL_RADIUS)/2 + BORDER_OFFSET_NORTH);
 
-    //Not yet the proper initial paddle location.
-    private final GOval ball = new GOval(BALL_RADIUS, BALL_RADIUS);
-    private final GRect brick = new GRect(BRICK_WIDTH, BRICK_HEIGHT);
-    private final GRect border = new GRect(BORDER_WIDTH,
-            BORDER_HEIGHT);
 
     private void initializeScreen(){
         setSize(SCREEN_WIDTH,SCREEN_HEIGHT);
@@ -63,10 +64,44 @@ public class Breakout extends GraphicsProgram {
     }
 
     private void initializeBall(){
-        ball.setLocation(canvasCenter);
+        int     x = (SCREEN_WIDTH - BALL_RADIUS)/2,
+                y = (SCREEN_HEIGHT - BALL_RADIUS)/2 + BORDER_OFFSET_NORTH;
+        ball.setLocation(x, y);
         ball.setFillColor(Color.WHITE);
         ball.setFilled(true);
         add(ball);
+    }
+
+    /**
+     * Helper method used in @@{initializeBrick()} to determine the color of
+     * the brick based on which row it is in.
+     * @param currentBrick
+     * @param row
+     */
+    private void determineBrickColor(GRect currentBrick, int row){
+        switch(row){
+            case 0:
+            case 1:
+                currentBrick.setFillColor(Color.RED);
+                break;
+            case 2:
+            case 3:
+                currentBrick.setFillColor(Color.ORANGE);
+                break;
+            case 4:
+            case 5:
+                currentBrick.setFillColor(Color.YELLOW);
+                break;
+            case 6:
+            case 7:
+                currentBrick.setFillColor(Color.GREEN);
+                break;
+            case 8:
+            case 9:
+                currentBrick.setFillColor(Color.CYAN);
+                break;
+        }
+        currentBrick.setFilled(true);
     }
 
     private void initializeBricks(){
@@ -74,53 +109,34 @@ public class Breakout extends GraphicsProgram {
 
         for (int row = 0; row < BRICKS_IN_ROW; row++) {
             if (row == 0) {
-                yCoord = BORDER_OFFSET_NORTH + 60;
-            } else{
-                yCoord = 60 + BORDER_OFFSET_NORTH + (row * ((BRICK_SPACING / 2) + BRICK_HEIGHT));
+                yCoord = BORDER_OFFSET_NORTH + BRICK_OFFSET_Y;
+            } else {
+                yCoord = BRICK_OFFSET_Y + BORDER_OFFSET_NORTH +
+                        (row * ((BRICK_SPACING / 2) + BRICK_HEIGHT));
             }
-
             for (int col = 0; col < BRICKS_IN_COL; col++) {
                 if(col == 0){
-                    xCoord =2 +  BORDER_OFFSET;
+                    xCoord = BORDER_OFFSET + BRICK_OFFSET_X;
                 }else {
-                    xCoord = 2 + BORDER_OFFSET + (col * (BRICK_SPACING + BRICK_WIDTH));
+                    xCoord = BORDER_OFFSET + BRICK_OFFSET_X +
+                            (col * (BRICK_SPACING + BRICK_WIDTH));
                 }
-
                 GRect currentBrick = new GRect(BRICK_WIDTH, BRICK_HEIGHT);
-                switch(row){
-                    case 0:
-                    case 1:
-                        currentBrick.setFillColor(Color.RED);
-                        break;
-                    case 2:
-                    case 3:
-                        currentBrick.setFillColor(Color.ORANGE);
-                        break;
-                    case 4:
-                    case 5:
-                        currentBrick.setFillColor(Color.YELLOW);
-                        break;
-                    case 6:
-                    case 7:
-                        currentBrick.setFillColor(Color.GREEN);
-                        break;
-                    case 8:
-                    case 9:
-                        currentBrick.setFillColor(Color.CYAN);
-                        break;
-                }
-                currentBrick.setFilled(true);
-                System.out.println(currentBrick.getFillColor());
+                determineBrickColor(currentBrick, row);
                 add(currentBrick, xCoord, yCoord);
             }
         }
-
     }
 
     private void initializeHeader(){
-
+        double x, y;
+        GLabel header = new GLabel("BREAKOUT");
+        header.setFont("Helvetica-20");
+        x = (SCREEN_WIDTH - header.getWidth()) / 2;
+        y = header.getHeight();
+        header.setLocation(x, y);
+        add(header);
     }
-
 
 
     private void initialize(){
@@ -129,6 +145,7 @@ public class Breakout extends GraphicsProgram {
         initializePaddle();
         initializeBall();
         initializeBricks();
+        initializeHeader();
     }
 
     public void run(){
