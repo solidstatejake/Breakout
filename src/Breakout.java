761b79298d1b1e4579a2ac41941b46c8d7631919
solidstatejake <jake.berg@sunydutchess.edu>
2019-05-22 14:44:14 -0400

    Create initializeHeader(), isCollidingWithPaddle(), isCollidingWithBrick(), brickCollisionHandler(), keyPressed(). Add Javadocs.
    - initializeHeader() creates the header and adds it to screen.
    - isCollidingWithPaddle() returns true if ball is colliding with paddle.
    - isCollidingWithBrick() returns true if ball is colliding with a brick.
    - brickCollisionHandler() removes a brick and alters ball velocity when ball hits a brick.
    -keyPressed() handles the left and right arrow keys for moving the paddle.

--- src/Breakout.java
+++ src/Breakout.java
@@ -1,14 +1,13 @@
+/**
+ *  
+ */
 
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
-import acm.program.Program;
 import acm.util.RandomGenerator;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
 
 
 public class Breakout extends GraphicsProgram {
@@ -30,7 +29,7 @@ public class Breakout extends GraphicsProgram {
     private final double INITIAL_PADDLE_X = (SCREEN_WIDTH - PADDLE_WIDTH) / 2;
     private final double INITIAL_PADDLE_Y = (SCREEN_HEIGHT - PADDLE_WIDTH)* 0.9;
     private final GRect paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
-
+    private final double PADDLE_SPEED_FACTOR = 0.1;
 
 
     private final int BALL_RADIUS           = 10;
@@ -45,27 +44,47 @@ public class Breakout extends GraphicsProgram {
     private final int BRICK_OFFSET_X        = 2;
     private final int BRICK_OFFSET_Y        = 60;
 
-
-    private final double CANVAS_CENTER_X = SCREEN_WIDTH / 2;
-    private final double CANVAS_CENTER_Y = SCREEN_HEIGHT / 2;
-
     private boolean gameover = false;
-    private double velocity_x,
-                velocity_y;
-    private double paddle_x;
+    private double velocity_x, velocity_y;
+    private int lives = 3;
 
 
-
+    /**
+     * Set the size of the screen and set the background color as GRAY.
+     */
     private void initializeScreen(){
         setSize(SCREEN_WIDTH,SCREEN_HEIGHT);
         setBackground(Color.GRAY);
     }
 
+    /**
+     * Initialize the "BREAKOUT" title at the top of the screen.
+     */
+    private void initializeHeader(){
+        double x, y;
+        GLabel header = new GLabel("BREAKOUT");
+        header.setFont("Helvetica-20");
+        x = (SCREEN_WIDTH - header.getWidth()) / 2;
+        y = header.getHeight();
+        header.setLocation(x, y);
+        add(header);
+    }
+
+
+    /**
+     * Create the border in which the actual game is played. Color it
+     * LIGHT_GRAY.
+     */
     private void initializeBorder(){
         border.setLocation(BORDER_OFFSET, BORDER_OFFSET_NORTH);
+        border.setFillColor(Color.LIGHT_GRAY);
+        border.setFilled(true);
         add(border);
     }
 
+    /**
+     * Create the paddle and color is BLACK.
+     */
     private void initializePaddle(){
         paddle.setLocation(INITIAL_PADDLE_X, INITIAL_PADDLE_Y);
         paddle.setFillColor(Color.BLACK);
@@ -73,21 +92,26 @@ public class Breakout extends GraphicsProgram {
         add(paddle);
     }
 
+    /**
+     * Create the ball and color is WHITE.
+     */
     private void initializeBall(){
         int     x = (SCREEN_WIDTH - BALL_RADIUS)/2,
                 y = (SCREEN_HEIGHT - BALL_RADIUS)/2 + BORDER_OFFSET_NORTH;
         ball.setLocation(x, y);
         ball.setFillColor(Color.WHITE);
         ball.setFilled(true);
-        assignInitalVelocities();
+        assignInitialVelocities();
         add(ball);
     }
 
     /**
      * Helper method used in @@{initializeBrick()} to determine the color of
      * the brick based on which row it is in.
-     * @param currentBrick
-     * @param row
+     * @param currentBrick is the current brick being created in the for-loop
+     *                     in initializeBrick().
+     * @param row is the current row that the for loop is on. There are 10 rows
+     *            and 10 columns of bricks.
      */
     private void determineBrickColor(GRect currentBrick, int row){
         switch(row){
@@ -115,6 +139,9 @@ public class Breakout extends GraphicsProgram {
         currentBrick.setFilled(true);
     }
 
+    /**
+     * Create and fill the bricks with color.
+     */
     private void initializeBricks(){
         int xCoord = 0, yCoord = 0;
 
@@ -139,16 +166,10 @@ public class Breakout extends GraphicsProgram {
         }
     }
 
-    private void initializeHeader(){
-        double x, y;
-        GLabel header = new GLabel("BREAKOUT");
-        header.setFont("Helvetica-20");
-        x = (SCREEN_WIDTH - header.getWidth()) / 2;
-        y = header.getHeight();
-        header.setLocation(x, y);
-        add(header);
-    }
 
+    /**
+     * Aggregate all initialization methods into one place.
+     */
     private void initialize(){
         initializeScreen();
         initializeBorder();
@@ -158,30 +179,53 @@ public class Breakout extends GraphicsProgram {
         initializeHeader();
     }
 
+    /**
+     * Moves the ball according to it's velocity_x and velocity_y factors.
+     */
     private void moveBall(){
         ball.setLocation(ball.getX() + velocity_x,ball.getY() + velocity_y);
     }
 
-    private void assignInitalVelocities(){
+    /**
+     * Randomly assign initial @velocity_x and @velocity_y factors.
+     */
+    private void assignInitialVelocities(){
         RandomGenerator rgen = new RandomGenerator();
-        velocity_x = rgen.nextDouble(-5, 5);
-        velocity_y = rgen.nextDouble(-5, 5);
+        do {
+            velocity_x = rgen.nextDouble(-5, 5);
+            velocity_y = rgen.nextDouble(-5, 5);
+        }while((velocity_x < 3) && (velocity_y < 3));
     }
 
+    /**
+     * Determine if the ball is colliding with the paddle.
+     * @return returns true if the ball is colliding with the paddle.
+     */
+    private boolean isCollidingWithPaddle(){
+        double ballX = ball.getX() + BALL_RADIUS;
+        double ballY = ball.getY() + BALL_RADIUS;
+        return (getElementAt(ballX, ballY) == paddle);
+    }
+
+    /**
+     * Determine if the ball is colliding with the left or right wall.
+     * @return Will return true if the ball is colliding with the left or right
+     *         wall.
+     */
     private boolean isCollidingWithHorizontalWall(){
         double  leftSideOfBall = ball.getX(),
                 rightSideOfBall = ball.getX() + (2 * BALL_RADIUS);
         int correctionFactor = 10, rightWall, leftWall;
         leftWall = BORDER_OFFSET;
         rightWall = (BORDER_OFFSET + BORDER_WIDTH + correctionFactor);
-
-        if (leftSideOfBall <= leftWall || rightSideOfBall  >= rightWall){
-            return true;
-        } else {
-            return false;
-        }
+        return (leftSideOfBall <= leftWall || rightSideOfBall >= rightWall);
     }
 
+    /**
+     * Determine if ball is colliding with the top or bottom wall.
+     * @return Will return true if the ball is colliding with the top or
+     *         bottom wall.
+     */
     private boolean isCollidingWithVerticalWall(){
         double  topOfBall = ball.getY(),
                 bottomOfBall = ball.getY() + (2 * BALL_RADIUS);
@@ -189,28 +233,38 @@ public class Breakout extends GraphicsProgram {
         topWall = BORDER_OFFSET_NORTH;
         bottomWall = BORDER_OFFSET_NORTH + BORDER_HEIGHT + correctionFactor;
 
-        if (topOfBall <= topWall || bottomOfBall >= bottomWall){
-            return true;
-        } else {
-            return false;
-        }
+        return topOfBall <= topWall || bottomOfBall >= bottomWall;
     }
 
-    public void keyPressed(KeyEvent event){
-        int key = event.getKeyCode();
-        switch(key){
-            case KeyEvent.VK_LEFT:
-                if((paddle.getX() - 0.05) > BORDER_OFFSET) {
-                    paddle.setLocation(paddle.getX() - 0.05, paddle.getY());
-                }
-                break;
-            case KeyEvent.VK_RIGHT:
-                if(paddle.getX() < SCREEN_WIDTH -
-                        BORDER_OFFSET - PADDLE_WIDTH + 0.05) {
-                    paddle.setLocation(paddle.getX() + 0.05, paddle.getY());
-                }
-        }
+    /**
+     * Determine if the ball is colliding with a brick.
+     * @return Will return true if the ball is not colliding with the border and
+     *         the ball is not colliding with the paddle. AKA the ball is
+     *         colliding with a brick.
+     */
+    private boolean isCollidingWithBrick(){
+        GObject collidingObject;
+        collidingObject = getElementAt(ball.getLocation());
+        return ((collidingObject != border) && (collidingObject != paddle));
     }
+
+    /**
+     * Handle collisions between the ball and bricks.
+     */
+    private void brickCollisionHandler(){
+        double ballTop = ball.getY();
+        double ballBottom = (ballTop + (2 * BALL_RADIUS));
+        double ballLeft = ball.getX();
+        double ballRight = (ballLeft + (2 * BALL_RADIUS));
+        GObject brick = getElementAt(ball.getLocation());
+        if ((brick.getX() <= ballLeft) || (brick.getX() >= ballRight)){
+            velocity_y = -velocity_y;
+        } else if((brick.getY() <= ballTop) || (brick.getY() >= ballBottom)){
+            velocity_x = -velocity_x;
+        }
+        remove(brick);
+    }
+
 
     /**
      * ballHandler() will change the velocity of the ball if it collides with
@@ -219,18 +273,46 @@ public class Breakout extends GraphicsProgram {
     private void ballHandler(){
         if (isCollidingWithHorizontalWall()){
             velocity_x = -velocity_x;
-        } else if (isCollidingWithVerticalWall()){
+        } else if (isCollidingWithVerticalWall()) {
             velocity_y = -velocity_y;
+        } else if (isCollidingWithPaddle()) {
+            velocity_y = -velocity_y;
+        } else if (isCollidingWithBrick()){
+                brickCollisionHandler();
         }
         moveBall();
     }
 
+    /**
+     * Register left-arrow-key and right-arrow-key events to move paddle.
+     * @param event
+     */
+    public void keyPressed(KeyEvent event){
+        int key = event.getKeyCode();
+        switch(key){
+            case KeyEvent.VK_LEFT:
+                if((paddle.getX() - PADDLE_SPEED_FACTOR) > BORDER_OFFSET) {
+                    paddle.setLocation(paddle.getX() - PADDLE_SPEED_FACTOR, paddle.getY());
+                }
+                break;
+            case KeyEvent.VK_RIGHT:
+                if(paddle.getX() < SCREEN_WIDTH -
+                        BORDER_OFFSET - PADDLE_WIDTH + PADDLE_SPEED_FACTOR) {
+                    paddle.setLocation(paddle.getX() + PADDLE_SPEED_FACTOR, paddle.getY());
+                }
+        }
+    }
+
+    /**
+     * Run initialize() to initialize screen-size, paddle, bricks, and ball.
+     * Run game loop.
+     */
     public void run(){
         initialize();
         while(!gameover){
             addKeyListeners();
             ballHandler();
-            pause(10);
+            pause(15);
         }
 
     }
