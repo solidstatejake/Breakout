95d70f89393f2c3bee4a0dd4f8d81e0431bdce8a
solidstatejake <jake.berg@sunydutchess.edu>
2019-05-21 20:02:24 -0400

    Create initialization method for ball. Simplify border constant names.

--- src/Breakout.java
+++ src/Breakout.java
@@ -5,12 +5,15 @@ import acm.program.GraphicsProgram;
 
 public class Breakout extends GraphicsProgram {
 
-    // Constants
+    // Screen
     private final int SCREEN_WIDTH          = 416;
     private final int SCREEN_HEIGHT         = 716;
 
-    private final int INNER_BORDER_WIDTH    = 400;
-    private final int INNER_BORDER_HEIGHT   = 600;
+    //Border
+    private final int BORDER_WIDTH = 400;
+    private final int BORDER_HEIGHT = 600;
+    private final int BORDER_NORTH_OFFSET = 86;
+    private final int BORDER_OFFSET = 8;
 
     private final int PADDLE_WIDTH          = 60;
     private final int PADDLE_HEIGHT         = 10;
@@ -19,34 +22,40 @@ public class Breakout extends GraphicsProgram {
     private final GRect paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
 
 
-
     private final int BALL_RADIUS           = 10;
 
     private final int BRICK_WIDTH           = 36;
     private final int BRICK_HEIGHT          = 8;
 
     private final int BALL_SPEED            = 10;
-    private final GPoint canvasCenter = new GPoint(getWidth()/2,
-                                                  getHeight()/2);
+    private final GPoint canvasCenter = new GPoint(
+            (SCREEN_WIDTH - BALL_RADIUS)/2,
+            (SCREEN_HEIGHT - BALL_RADIUS)/2 + BORDER_NORTH_OFFSET);
 
     //Not yet the proper initial paddle location.
     private final GOval ball = new GOval(BALL_RADIUS, BALL_RADIUS);
     private final GRect brick = new GRect(BRICK_WIDTH, BRICK_HEIGHT);
-    private final GRect innerBorder = new GRect(INNER_BORDER_WIDTH,
-                                                INNER_BORDER_HEIGHT);
+    private final GRect innerBorder = new GRect(BORDER_WIDTH,
+            BORDER_HEIGHT);
 
     private void initializeScreen(){
         setSize(SCREEN_WIDTH,SCREEN_HEIGHT);
     }
 
     private void initializeInnerBorder(){
-        innerBorder.setLocation(8, 86);
+        innerBorder.setLocation(BORDER_OFFSET,
+                BORDER_NORTH_OFFSET);
         add(innerBorder);
     }
 
     private void initializePaddle(){
         paddle.setLocation(INITIAL_PADDLE_X, INITIAL_PADDLE_Y);
         add(paddle);
+    }
+
+    private void initializeBall(){
+        ball.setLocation(canvasCenter);
+        add(ball);
     }
 
     private void intitializeBricks(){
@@ -57,14 +66,13 @@ public class Breakout extends GraphicsProgram {
 
     }
 
-    private void inititalizeBall(){
 
-    }
 
     private void initialize(){
         initializeScreen();
         initializeInnerBorder();
         initializePaddle();
+        initializeBall();
     }
 
     public void run(){
